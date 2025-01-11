a5a926d292a949331ccbf9b315a28288870991f4
Xinyu-Sheng <sheng.xin.yu@foxmail.com>
2025-01-11 18:28:02 +0800

    change to  your_fancy_extension_name

--- exts/your_fancy_extension_name/setup.py
+++ exts/your_fancy_extension_name/setup.py
@@ -1,4 +1,4 @@
-"""Installation script for the 'ext_template' python package."""
+"""Installation script for the 'your_fancy_extension_name' python package."""
 
 import os
 import toml
@@ -18,8 +18,8 @@ INSTALL_REQUIRES = [
 
 # Installation operation
 setup(
-    name="ext_template",
-    packages=["ext_template"],
+    name="your_fancy_extension_name",
+    packages=["your_fancy_extension_name"],
     author=EXTENSION_TOML_DATA["package"]["author"],
     maintainer=EXTENSION_TOML_DATA["package"]["maintainer"],
     url=EXTENSION_TOML_DATA["package"]["repository"],
--- exts/your_fancy_extension_name/your_fancy_extension_name/tasks/locomotion/velocity/config/anymal_d/rough_env_cfg.py
+++ exts/your_fancy_extension_name/your_fancy_extension_name/tasks/locomotion/velocity/config/anymal_d/rough_env_cfg.py
@@ -1,6 +1,6 @@
 from omni.isaac.lab.utils import configclass
 
-from ext_template.tasks.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
+from your_fancy_extension_name.tasks.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
 
 ##
 # Pre-defined configs
